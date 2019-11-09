c82019c764bf2869709c5fb60889fb1add696beb
georgemosch <jkr44@Georgioss-MBP.broadband>
2019-11-09 21:31:01 +0000

    Project bookmark initialized

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* eslint-disable require-jsdoc */
+// Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+function saveBookmark() {
+  console.log('it works');
+}
